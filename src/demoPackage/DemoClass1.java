3d3908095c7847b5549a20cfe51aa6b2354d9faa
tilak <tilak@LAPTOP-2122UIPD>
2023-07-26 16:20:32 +0530

    First Commit to Github from Eclipse

---
+++ src/demoPackage/DemoClass1.java
@@ -0,0 +1,10 @@
+package demoPackage;
+
+public class DemoClass1 {
+
+	public static void main(String[] args) {
+		System.out.println("This is Demo Class for Git Push Pull from to Github");
+		System.out.println("Using Eclipse IDE");
+	}
+
+}
